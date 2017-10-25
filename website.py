bdebeba61a233b7e9c8769c5d7d18625c998d4c4
willschwarzer <schwarzerw@carleton.edu>
2017-10-25 11:01:59 -0500

    last fix pre-presentation

--- website.py
+++ website.py
@@ -1,19 +1,8 @@
-#!/usr/bin/env python3
-'''
-    example_flask_app.py
-    Jeff Ondich, 22 April 2016
-    Modified by Eric Alexander, January 2017
-
-    A slightly more complicated Flask sample app than the
-    "hello world" app found at http://flask.pocoo.org/.
-'''
 import flask
 from flask import render_template
-import json
 import sys
 import datasource
 import numpy as np
-import time
 
 app = flask.Flask(__name__)
 
@@ -25,23 +14,22 @@ def hello():
 def get_results_1_variable(var):
     ds = datasource.DataSource()
     validVars = np.load('varList.npy')
-    print(var)
     if var not in validVars:
-        return render_template('index.html')
+        return render_template('error.html')
     else:
         ds.runQuery([var, ""])
         ds.createGraph()
         queryStats = ds.getStats()
         return render_template('results.html', 
                                 stats=queryStats, 
-                                vars = [var, ""])
+                                var1 = var, var2 = "")
                                 
 @app.route('/results/<var1>/<var2>')
 def get_results_2_variables(var1, var2):
     ds = datasource.DataSource()
     validVars = np.load('varList.npy')
     if var1 not in validVars or var2 not in validVars:
-        return render_template('index.html')
+        return render_template('error.html')
     else:
         ds.runQuery([var1, var2])
         ds.createGraph()
